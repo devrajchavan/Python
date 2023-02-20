49e5f04534c18fe18d7bb0c58b2cef2bad584681
devrajchavan <90541993+devrajchavan@users.noreply.github.com>
2023-02-20 10:58:50 +0530

    Create Object_Oriented_Programming.py

---
+++ Object_Oriented_Programming.py
@@ -0,0 +1,87 @@
+# LOCAL VARIABLE =
+# INSTANCE VARAIBLE + 
+
+
+
+# EXAMPLE 1
+
+
+'''class Student:
+    def __init__(self):   #This is a Constructor which have a default parameter as self
+        self.s_name = input("Enter your name")  
+        self.s_rollno = 101   #Instance Variable aka Data Member
+    def getdata(self):
+        self.s_mb = 9090909090
+obj = Student()     
+obj.getdata()
+obj.s_branch = "E&TC"  #Here we are declaring this value outside the class using the object
+print(obj.s_name)
+#del obj.s_rollno
+print(obj.__dict__)
+del obj.s_rollno
+print(obj.__dict__)
+'''
+
+
+
+# Example 2
+
+
+'''class Student:
+    def __init__(self):
+        self.s_name = "Devraj"
+        self.l_name = "Chavan"
+        self.s_rollno = 21
+        self.s_branch = "E&TC"
+        self.s_mb = 9090909090
+obj = Student()
+
+print(obj.__dict__)'''
+
+
+# Example 3
+
+'''class Hod:
+    def __init__(self):
+        self.name="DEVRAJ"
+        self.age=20
+        self.rollno=21
+
+    def info(self):
+        print("My Name is:", self.name)
+        print("My Age is: ", self.age)
+        print("My emp id:", self.rollno)
+
+obj = Hod()
+
+obj.info()'''
+
+
+
+#Example 4
+
+class College:
+    collegename="DYPIT"   #This is a static variable as it is outside the method and inside the class 
+
+    def __init__(self):
+        self.studentname= "Gaurav"            #Here studentname is a local vairable
+
+principal = College()
+teacher = College()
+accountant = College()
+
+principal.studentname="Pratik"
+# teacher.studentname="Devraj"
+
+print("Principal",principal.collegename,"...",principal.studentname)
+print("Teacher", teacher.collegename,"...",teacher.studentname)
+print("accountant", accountant.collegename,"...",accountant.studentname)
+
+print("---------------------------------------------------")
+
+College.collegename="DY AKURDI" #using the classname we can access the static VARIABLE
+principal.studentname="Devraj CHavan"
+
+print("principal =", principal.collegename,"|",principal.studentname)
+print("teacher ",teacher.collegename,"|",teacher.studentname)
+print("accountant",accountant.collegename,"|",accountant.studentname)
